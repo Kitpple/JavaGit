4555eb4c6566d26348d84fbf7446b1d65bdcbac1
SY <SY@DESKTOP-LF2EHBU>
2024-02-10 18:04:11 +0900

    Chapter6 commit

---
+++ src/Chapter6/Birthday.java
@@ -0,0 +1,31 @@
+package Chapter6;
+
+ class ThisExample {
+	int day;
+	int maonth;
+	int year;
+	
+	public void setYear(int year) {
+		this.year = year ;
+	}
+	
+	public void printThis() {
+		System.out.println(this);
+	}
+
+}
+
+public class  Birthday {
+	public static void main (String[ ] args) {
+		
+		ThisExample bDay = new ThisExample();
+		bDay.setYear(2000);
+		System.out.println(bDay);
+		bDay.printThis();
+	}
+
+		
+	}
+	
+	
+
